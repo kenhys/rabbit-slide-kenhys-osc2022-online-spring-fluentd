d48b3346cfdbee6ad668852d56c0ea53edd84bc8
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-11 21:24:14 +0900

    Tweak slide title font size

--- theme.rb
+++ theme.rb
@@ -1,7 +1,9 @@
 @default_font = "Ubuntu"
 @monospace_font = "Ubuntu"
 # Use smaller title font size
-@x_large_font_size = @x_large_font_size * 0.8
+@x_large_font_size = @x_large_font_size * 0.7
+# Use smaller slide title font size
+@large_font_size = @x_large_font_size * 0.7
 include_theme("lightning-clear-blue")
 @slide_show_loop = true
 
